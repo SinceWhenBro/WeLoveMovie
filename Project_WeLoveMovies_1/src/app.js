c5401223c6ba13e271abe05b01b970f4f6cb7e9c
Tyler Williams <tylerwilliams1737@gmail.com>
2021-10-18 12:45:47 -0700

    Added: Error Handling/Migrations

--- Project_WeLoveMovies_1/src/app.js
+++ Project_WeLoveMovies_1/src/app.js
@@ -1,5 +1,12 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const errorHandler = require("./errors/errorHandler");
+
+app.use(express.json());
+app.use(cors());
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
